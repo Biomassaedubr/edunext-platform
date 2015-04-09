dcb6c45ab1f71a56c30fffe863f60e9bcc028570
Laq <leonardo.quinonez@edunext.co>
2015-04-09 15:42:59 -0500

    test with just paver

--- info.sh
+++ info.sh
@@ -1,16 +1,20 @@
-!#/bin/bash
-ls
-pwd
-echo ".."
-ls -al ../
-echo "../.."
-ls -al ../../
-echo "../../.."
-ls -al ../../../
+#!/bin/bash
+# ls
+# pwd
+# echo ".."
+# ls -al ../
+# echo "../.."
+# ls -al ../../
+# echo "../../.."
+# ls -al ../../../
 python --version
-pip list
-env
+cat /home/travis/virtualenv/python2.7_with_system_site_packages/local/lib/python2.7/site-packages/paver/deps/six.py
+cat /home/travis/virtualenv/python2.7_with_system_site_packages/local/lib/python2.7/site-packages/six.py
+# pip list
+# env
 
+echo "importing"
 echo "import urllib2" | python 
 echo "import urllib" | python 
+echo "imported right"
 
